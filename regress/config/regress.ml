0cd67975fea6a7b39082645d8202b47933744487
Anil Madhavapeddy <anil@recoil.org>
2008-01-02 10:57:47 +0000

    update config regression test to show the results of a command line override

--- regress/config/regress.ml
+++ regress/config/regress.ml
@@ -8,29 +8,33 @@ type result =
 	|Fail of int
 	
 let regress_files =
+    let blank_fun = (fun (_:Config.config) -> ()) in
     let def = {t_short=None; t_long=None; t_name="";
         t_atom=T_unknown; t_descr=None; t_default=None} in [
-	("strings.conf", Pass,
+    ("strings.conf", Pass,
 	  [
-		{def with t_name="config1"; t_atom=T_string; t_descr=None; t_default=None};
+		{def with t_short=Some 's'; t_name="config1"; t_atom=T_string; t_descr=None; t_default=Some (V_string "foo")};
 		{def with t_name="config2"; t_atom=T_string; t_descr=None; t_default=None};
 		{def with t_name="config3"; t_atom=T_string; t_descr=None; t_default=None};
 		{def with t_name="config4.blah"; t_atom=T_string; t_descr=None; t_default=None};
 		{def with t_name="config4.blah2"; t_atom=T_string; t_descr=None; t_default=None};
-  	  ]
-    );
+  	  ],
+         (fun conf ->
+            print_endline (sprintf "POST: strings.conf, config1= %s" (string_of_var_val (conf#get_val "config1")))
+         )
+        );
     ("variants.conf", Pass,
 	  [
 		{def with t_name="config1"; t_atom=(T_variant ["Foo";"Bar";"Alpha"]); t_descr=None; t_default=None};
 		{def with t_name="config2"; t_atom=(T_variant ["Bar";"Foo"]); t_descr=None; t_default=None};
 		{def with t_name="config3"; t_atom=(T_variant_list ["Foo";"Bar";"Alpha"]); t_descr=None; t_default=None};
 		{def with t_name="config4"; t_atom=(T_variant_list ["Foo";"Bar";"Alpha"]); t_descr=None; t_default=None};
-	  ]
+	  ], blank_fun
 	);
     ("variants.conf", (Fail 3),
 	  [
 		{def with t_name="config1"; t_atom=(T_variant ["Bar";"Alpha"]); t_descr=None; t_default=None};
-	  ]
+	  ], blank_fun
 	);
     ("ip.conf", Pass,
 	  [
@@ -39,9 +43,8 @@ let regress_files =
 		{def with t_name="config3"; t_atom=T_ip_list; t_descr=None; t_default=Some (V_ip (Unix.inet_addr_of_string "5.6.7.7"))};
 		{def with t_name="config4"; t_atom=T_ip_list; t_descr=None; t_default=None};
 		{def with t_name="config5"; t_atom=T_ip_list; t_descr=None; t_default=None};
- 	  ]
-	);
- 
+ 	  ], blank_fun
+    );
 ]
 
 (* ex=expected result, ac=actual result *)
@@ -57,13 +60,20 @@ let result file ex ac =
 	|Fail x,Pass -> no (sprintf "Expected fail at %d, actually passed" x)
 		
 let _ =
-    List.iter (fun (file,res,ty) ->
+    (* If REGRESS_CMDLINE is set, then only parse strings.conf *)
+    let regress_files = try
+        ignore(Sys.getenv("REGRESS_CMDLINE"));
+        [List.hd regress_files]
+      with
+        Not_found -> regress_files
+    in
+    List.iter (fun (file,res,ty,postfn) ->
         printf "===> Testing: %s\n" file;
 		try
 		let v = new Config.config ty file in
 		v#dump;
-	    print_endline (Config.default_config ty);
 		result file res Pass;
+                postfn v;
 		with
 		Config.Error (l,s) -> print_endline s; result file res (Fail l)
     ) regress_files
